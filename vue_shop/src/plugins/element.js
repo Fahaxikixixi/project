ba92de955fcc4e598d9cfb0634c255204f3eca2a
Fahaxiki <1062348882@qq.com>
2020-10-12 08:47:09 +0800

    提交

--- vue_shop/src/plugins/element.js
+++ vue_shop/src/plugins/element.js
@@ -1,6 +1,31 @@
 import Vue from 'vue'
 
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import {
+    Button,
+    Form,
+    FormItem,
+    Input,
+    Message,
+    Container,
+    Header,
+    Aside,
+    Main,
+    Menu,
+    Submenu,
+    MenuItemGroup,
+    MenuItem,
+    Breadcrumb,
+    BreadcrumbItem,
+    Card,
+    Row,
+    Col,
+    Table,
+    TableColumn,
+    Switch,
+    Tooltip,
+    Pagination,
+    Dialog
+} from 'element-ui'
 
 //导入弹框提示组件  Message
 
@@ -10,5 +35,26 @@ Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
-    //Message 需要全局挂载，$message自定义属性
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItemGroup)
+Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
+
+
+//Message 需要全局挂载，$message自定义属性
 Vue.prototype.$message = Message
